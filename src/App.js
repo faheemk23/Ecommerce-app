faf89d196480965e95740c4434bb8996ecab8717
faheemk23 <faheemwork23@gmail.com>
2023-05-11 18:39:06 +0530

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+// import axios from "axios"
 
 import "./App.css"
 
 function App() {
   return (
     <div>
-      Ecommerce App + test deployment
+      Ecommerce App 
     </div>
   )
 }
