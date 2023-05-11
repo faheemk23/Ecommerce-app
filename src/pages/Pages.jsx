aee395459d5a13a42bdf4fbc9b0830d51fc12486
faheemk23 <faheemwork23@gmail.com>
2023-05-11 19:39:10 +0530

    added pages

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,10 @@
+import { Home } from "./Home";
+import { ProductListing } from "./ProductListing";
+import { ProductDetail } from "./ProductDetail";
+import { Cart } from "./Cart";
+import { Wishlist } from "./Wishlist";
+import { SignUp } from "./SignUp";
+import { Login } from "./Login";
+import { Checkout } from "./Checkout";
+
+export {Home,ProductListing,ProductDetail,Cart,Wishlist,SignUp,Login,Checkout}
---
+++ src/pages/ProductListing.jsx
@@ -0,0 +1,3 @@
+export function ProductListing(){
+    return(<>ProductListing</>)
+}
