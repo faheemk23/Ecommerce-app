7156e794d5e42bdd4ec63c77a6819f590efaee45
faheemk23 <faheemwork23@gmail.com>
2023-05-11 19:39:59 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {Routes,Route} from "react-router-dom";
 // import axios from "axios"
 
 import "./App.css"
+import { Home,ProductListing,ProductDetail,Cart, Wishlist, SignUp, Login, Checkout } from './pages/Pages';
+
 
 function App() {
   return (
     <div>
-      Ecommerce App 
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/productlisting' element={<ProductListing/>}/>
+        <Route path='/productdetail' element={<ProductDetail/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/wishlist' element={<Wishlist/>} />
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/checkout' element={<Checkout/>} />
+      </Routes>
     </div>
   )
 }
