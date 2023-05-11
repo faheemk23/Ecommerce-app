65db3d2a5e7528405db887eff1f0d586d895b507
faheemk23 <faheemwork23@gmail.com>
2023-05-12 01:17:28 +0530

    chore: mockBee data

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,4 +1,4 @@
-import { v4 as uuid } from "uuid";
+// import { v4 as uuid } from 'uuid';
 
 /**
  * Category Database can be added here.
@@ -7,21 +7,18 @@ import { v4 as uuid } from "uuid";
 
 export const categories = [
   {
-    _id: uuid(),
-    categoryName: "fiction",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+    _id: '68a20c94-47ec-43ad-be3e-d31722a2cee9',
+    categoryName: 'Men',
+    description: '',
   },
   {
-    _id: uuid(),
-    categoryName: "non-fiction",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    _id: '7543d803-3418-42af-b384-dea65d601302',
+    categoryName: 'Women',
+    description: '',
   },
   {
-    _id: uuid(),
-    categoryName: "horror",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    _id: '46c210cf-3efa-46b7-bdaa-73d001d7b6ab',
+    categoryName: 'Kids',
+    description: '',
   },
 ];
