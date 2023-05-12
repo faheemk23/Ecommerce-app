d7a1771b2c323bd3a8c80e8ba521b005cdc6cc8a
faheemk23 <faheemwork23@gmail.com>
2023-05-12 06:18:35 +0530

    feat: showing filter column

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,8 +1,10 @@
 import { Navigation } from "../components/nav/Navigation";
+import { FilterColumn } from "../components/filters/FilterColumn";
 
 export function Wishlist(){
     return(<>
     <Navigation/>
     Wishlist
+    <FilterColumn/>
     </>)
 }
