e7255f36d223a6aed5c208d8c00399ac8f310a7d
faheemk23 <faheemwork23@gmail.com>
2023-05-12 08:37:24 +0530

    removed component

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -1,10 +1,8 @@
-// import { FilterColumn } from "../components/filters/FilterColumn";
 import { Navigation } from "../components/nav/Navigation";
 
 export function ProductListing(){
     return(<>
     <Navigation/>
     ProductListing
-    {/* <FilterColumn/> */}
     </>)
 }
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,10 +1,8 @@
 import { Navigation } from "../components/nav/Navigation";
-// import { FilterColumn } from "../components/filters/FilterColumn";
 
 export function Wishlist(){
     return(<>
     <Navigation/>
     Wishlist
-    {/* <FilterColumn/> */}
     </>)
 }
