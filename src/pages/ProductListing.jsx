ea3b2c211dbb01a92e01eeb899c5a3b7456ae691
faheemk23 <faheemwork23@gmail.com>
2023-05-12 18:32:43 +0530

    feat: added product listing

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -1,10 +1,24 @@
+import { useContext } from "react";
 import { Navigation } from "../components/nav/Navigation";
+import { ProductsContext } from "../contexts/ProductsProvider";
+import { ProductCard } from "../components/cards/ProductCard";
+import "./ProductListing.css";
 
 export function ProductListing() {
+  const { products } = useContext(ProductsContext);
   return (
-    <>
+    <div className="listing">
       <Navigation />
-      ProductListing
-    </>
+      <div className="filters">
+        <div>
+          <strong>Filters</strong> <button>Clear</button>
+        </div>
+      </div>
+      <div className="products">
+        {products.map((item) => (
+          <ProductCard key={item.id} />
+        ))}
+      </div>
+    </div>
   );
 }
