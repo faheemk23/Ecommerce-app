cffaf18be8121b4ae3e2f9da08411695f1d6a3a4
faheemk23 <faheemwork23@gmail.com>
2023-05-12 08:47:46 +0530

    feat: added filter column component

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,8 +1,10 @@
+import { FilterColumn } from "../components/filters/FilterColumn";
 import { Navigation } from "../components/nav/Navigation";
 
 export function Wishlist(){
     return(<>
     <Navigation/>
     Wishlist
+    <FilterColumn/>
     </>)
 }
