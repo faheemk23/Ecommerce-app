4e69520b4a38d1ad0fdc98d243ee817f378c6fc6
faheemk23 <faheemwork23@gmail.com>
2023-05-12 08:35:50 +0530

    removed component

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -1,10 +1,10 @@
-import { FilterColumn } from "../components/filters/FilterColumn";
+// import { FilterColumn } from "../components/filters/FilterColumn";
 import { Navigation } from "../components/nav/Navigation";
 
 export function ProductListing(){
     return(<>
     <Navigation/>
     ProductListing
-    <FilterColumn/>
+    {/* <FilterColumn/> */}
     </>)
 }
