92cdf3838154d271c5df7d169482ec1f1898031e
faheemk23 <faheemwork23@gmail.com>
2023-05-12 06:24:33 +0530

    bug: build failing

--- src/components/Filters/FilterColumn.jsx
+++ src/components/Filters/FilterColumn.jsx
@@ -1,18 +1,18 @@
-import { Categories } from "./Categories";
-import { Price } from "./Price";
-import { Ratings } from "./Ratings";
-import { Sort } from "./Sort";
+// import { Categories } from "./Categories";
+// import { Price } from "./Price";
+// import { Ratings } from "./Ratings";
+// import { Sort } from "./Sort";
 
-export function FilterColumn(){
-    return(<>
-    <h2>Filters</h2>
-    <button>clear</button>
-    <Price/>
-    <h2>Category</h2>
-    <Categories/>
-    <h2>Rating</h2>
-    <Ratings/>
-    <Sort/>
+// export function FilterColumn(){
+//     return(<>
+//     <h2>Filters</h2>
+//     <button>clear</button>
+//     <Price/>
+//     <h2>Category</h2>
+//     <Categories/>
+//     <h2>Rating</h2>
+//     <Ratings/>
+//     <Sort/>
     
-    </>)
-}
+//     </>)
+// }
