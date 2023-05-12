e9cec9afaf505497542eef043e5429d2ce027b0c
faheemk23 <faheemwork23@gmail.com>
2023-05-12 06:06:31 +0530

    refactor: changed 'home' prop to 'showBtnLogin'

--- src/components/nav/Navigation.jsx
+++ src/components/nav/Navigation.jsx
@@ -3,10 +3,10 @@ import "./Navigation.css"
 import { SearchBar } from "./SearchBar"
 import { NavRightPart } from "./NavRightPart"
 
-export function Navigation({home}){
+export function Navigation({showBtnLogin}){
     return(<div className="nav">
         <div className="nav-logo"><Link to='/'>BazaarShoppingSite</Link></div>
         <SearchBar/>
-        <NavRightPart home={home}/>
+        <NavRightPart showBtnLogin={showBtnLogin}/>
     </div>)
 }
