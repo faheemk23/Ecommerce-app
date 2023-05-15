0987a0beae6d4bc08239b74963fc4fca9a52b7eb
faheemk23 <faheemwork23@gmail.com>
2023-05-15 07:32:26 +0530

    feat: dynamic product listing

--- src/components/cards/ProductCard/ProductCard.jsx
+++ src/components/cards/ProductCard/ProductCard.jsx
@@ -1,15 +1,37 @@
+import { handleBtnAddToCart } from "../../../Utilites/cartUtilities";
 import "./ProductCard.css";
 
-export function ProductCard() {
+export function ProductCard({ product, cart }) {
+  const { image, title, original_price, price } = product;
+  const discount = parseInt(
+    ((Number(original_price, 0) - Number(price, 0)) /
+      Number(original_price, 0)) *
+      100
+  );
+  console.log(product);
   return (
     <div style={{ backgroundColor: "rgba(0,0,0,0.2)", maxWidth: "200px" }}>
-      <img src="https://picsum.photos/200/200" alt="product" />
-      <div>Men Premium Jacket</div>
+      <img src={image} alt="product" height="200px" width="200px" />
+      <div>{title}</div>
       <div>
-        <strong>₹2000</strong> <span className="original-price">₹3999</span>
+        <strong>₹{price}</strong>{" "}
+        <span className="original-price">₹{original_price}</span>
       </div>
-      <div>50% off</div>
-      {true ? <button>Add to Cart</button> : <button>Go to Cart</button>}
+      <div>{discount}% off</div>
+      {cart ? (
+        <button>Remove from cart</button>
+      ) : (
+        <>
+          {true ? (
+            <button onClick={() => handleBtnAddToCart(product)}>
+              Add to Cart
+            </button>
+          ) : (
+            <button>Go to Cart</button>
+          )}
+        </>
+      )}
+
       {true ? (
         <button>Save to Wishlist</button>
       ) : (
