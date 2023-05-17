6e1da58dfd07a7b16cd724308158f9dbf51af724
faheemk23 <faheemwork23@gmail.com>
2023-05-17 22:51:33 +0530

    feat: redirection on clicking nav logo

--- src/components/nav/Navigation.jsx
+++ src/components/nav/Navigation.jsx
@@ -3,11 +3,13 @@ import { SearchBar } from "./SearchBar";
 import { NavRightPart } from "./NavRightPart";
 
 import navLogo from "../../assets/nav-image.jpg";
+import { useNavigate } from "react-router-dom";
 
 export function Navigation({ showBtnLogin }) {
+  const navigate = useNavigate();
   return (
     <div className="nav">
-      <img src={navLogo} />
+      <img className="nav-logo" src={navLogo} onClick={() => navigate("/")} />
       <div className="nav-logo"></div>
       <SearchBar />
       <NavRightPart showBtnLogin={showBtnLogin} />
