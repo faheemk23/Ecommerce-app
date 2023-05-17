b7f0b0fa02b71449dcb01df25e4b6f227ae1cf45
faheemk23 <faheemwork23@gmail.com>
2023-05-17 19:01:07 +0530

    feat: added hero on home

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import heroImage from "../../assets/fruits-hero.webp";
+import "./Hero.css";
+
+export function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero-heading">
+        <h2>
+          Order Your
+          <br />
+          Daily Groceries
+        </h2>
+
+        <h3 className="white">#Free delivery</h3>
+        <Link to="/productlisting" className="hero-link">
+          Show Products
+        </Link>
+      </div>
+      <img className="hero-img" src={heroImage} alt="food" />
+    </div>
+  );
+}
