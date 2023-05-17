a4f600e0a8954604f716d28562f4d24d4d81cc41
faheemk23 <faheemwork23@gmail.com>
2023-05-17 19:01:31 +0530

    style: search bar

--- src/components/nav/SearchBar.jsx
+++ src/components/nav/SearchBar.jsx
@@ -1,7 +1,16 @@
+import { useContext } from "react";
+import { ProductsListingContext } from "../../contexts/ProductsListingContext";
+
 export function SearchBar() {
+  const { products } = useContext(ProductsListingContext);
+  const totalProductsNum = products.length;
   return (
     <>
-      <input className="search-bar" type="text" placeholder="search bar" />
+      <input
+        className="search-bar"
+        type="text"
+        placeholder={`Search ${totalProductsNum}+ products `}
+      />
     </>
   );
 }
