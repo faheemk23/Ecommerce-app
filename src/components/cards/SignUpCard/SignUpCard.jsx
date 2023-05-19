b402beaeaf704bdd4b0a38644f98915be049583f
faheemk23 <faheemwork23@gmail.com>
2023-05-19 22:35:32 +0530

    feat: show/hide pass word state+ icon

--- src/components/cards/SignUpCard/SignUpCard.jsx
+++ src/components/cards/SignUpCard/SignUpCard.jsx
@@ -8,6 +8,12 @@ export function SignUpCard() {
     password: "",
   });
 
+  const [showPassword, setShowPassword] = useState(true);
+
+  const toggleShowPassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   const handleSignupEmailField = (e) => {
     const userEmail = e.target.value;
     setSignupData((prev) => ({ ...prev, email: userEmail }));
@@ -22,6 +28,14 @@ export function SignUpCard() {
       <h2>Sigup</h2>
 
       <div>
+        <label htmlFor="first-name">First name: </label>
+        <input type="text" id="first-name" required />
+      </div>
+      <div>
+        <label htmlFor="last-name">Last name: </label>
+        <input type="text" id="last-name" required />
+      </div>
+      <div>
         <label htmlFor="email">Email address: </label>
         <input
           type="text"
@@ -32,12 +46,25 @@ export function SignUpCard() {
       </div>
       <div>
         <label htmlFor="password">Password: </label>
+
         <input
-          type="text"
+          type="password"
           id="password"
           onChange={handleSignupPasswordField}
           required
         />
+        {showPassword ? (
+          <i
+            className="fa-sharp fa-regular fa-eye-slash"
+            onClick={toggleShowPassword}
+          ></i>
+        ) : (
+          <i className="fa-regular fa-eye" onClick={toggleShowPassword}></i>
+        )}
+      </div>
+      <div>
+        <label htmlFor="confirm-password">Confirm password: </label>
+        <input type="password" id="confirm-password" required />
       </div>
       <div>
         <input type="checkbox" id="accept-tc" required />
