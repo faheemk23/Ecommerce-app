735f9aada92d64ce0dc6b232b097ca34939234cb
faheemk23 <faheemwork23@gmail.com>
2023-05-19 14:20:42 +0530

    feat: removed delivery charges

--- src/components/cards/PriceDetailCard/PriceDetailCard.jsx
+++ src/components/cards/PriceDetailCard/PriceDetailCard.jsx
@@ -2,17 +2,15 @@ import "./PriceDetailCard.css";
 
 export function PriceDetailCard({ cart }) {
   const priceDetails = cart.reduce(
-    (acc, { price, original_price, delivery_time, qty }) => ({
+    (acc, { price, original_price, qty }) => ({
       price: acc.price + Number(original_price) * qty,
       discount: acc.discount + qty * (Number(original_price) - Number(price)),
       items: acc.items + qty,
-      delivery_time: acc.delivery_time + Number(delivery_time),
     }),
-    { price: 0, items: 0, discount: 0, delivery_time: 0 }
+    { price: 0, items: 0, discount: 0 }
   );
 
-  const { price, items, discount, delivery_time } = priceDetails;
-  const delivery_charges = delivery_time * 15;
+  const { price, items, discount } = priceDetails;
 
   return (
     <div style={{ backgroundColor: "rgba(0,0,0,0.2)", maxWidth: "250px" }}>
@@ -29,25 +27,13 @@ export function PriceDetailCard({ cart }) {
             <td>-₹{discount}</td>
           </tr>
           <tr>
-            <th>Delivery Charges</th>
-            <td>{price > 5000 ? "Free" : <>+₹{delivery_charges}</>}</td>
-          </tr>
-          <tr>
             <th>Total Amount</th>
-            <td>
-              ₹
-              {price > 5000
-                ? price - discount
-                : price - discount + delivery_charges}
-            </td>
+            <td>₹{price - discount}</td>
           </tr>
         </tbody>
       </table>
       <hr />
-      <div>
-        You will save ₹{price > 5000 ? discount + delivery_charges : discount}{" "}
-        on this order
-      </div>
+      <div>You will save ₹{discount} on this order</div>
       <button>PLACE ORDER</button>
     </div>
   );
