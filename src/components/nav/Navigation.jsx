537ba920793af42f84ec9550dc837e243cc296ed
faheemk23 <faheemwork23@gmail.com>
2023-05-21 19:31:39 +0530

    refactor: removing unnecessary imports

--- src/components/nav/Navigation.jsx
+++ src/components/nav/Navigation.jsx
@@ -1,15 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 import "./Navigation.css";
 import { NavRightPart } from "./NavRightPart";
-
 import navLogo from "../../assets/nav-image.jpg";
-import { useNavigate } from "react-router-dom";
-import { useContext, useState } from "react";
-import { ProductsListingContext } from "../../contexts/ProductsListingContext";
 import { SearchContainer } from "./SearchContainer";
 
 export function Navigation({ showBtnLogin }) {
-  const { filtersState } = useContext(ProductsListingContext);
-
   const navigate = useNavigate();
 
   return (
