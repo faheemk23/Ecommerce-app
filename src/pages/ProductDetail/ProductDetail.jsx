863bba1e0d77f506c96f9a97df1e057d11f99b3d
faheemk23 <faheemwork23@gmail.com>
2023-05-21 19:29:06 +0530

    feat: navigate to detail page on  info click

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,10 +1,39 @@
+import { useParams } from "react-router-dom";
 import { Navigation } from "../../components/nav/Navigation";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { ProductCard } from "../../components/cards/ProductCard/ProductCard";
 
 export function ProductDetail() {
+  const [product, setProduct] = useState({});
+  const [loading, setLoading] = useState(true);
+  const { productId } = useParams();
+  const fetchProductDetail = async (productId, setProduct) => {
+    try {
+      const res = await axios.get(`/api/products/${productId}`);
+      if (res.status === 200) {
+        setProduct(res.data.product);
+      }
+    } catch (e) {
+      console.error({
+        message: e.message,
+        code: e.code,
+        where: "fetchProductDetailHandler",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProductDetail(productId, setProduct);
+  }, []);
+
   return (
     <>
       <Navigation />
-      ProductDetail
+      {loading && <p>Loading...</p>}
+      {!loading && <ProductCard product={product} />}
     </>
   );
 }
