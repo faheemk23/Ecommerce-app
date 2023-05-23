471a430602298ef86d4355fdce5e5aaee291406a
faheemk23 <faheemwork23@gmail.com>
2023-05-23 16:19:46 +0530

    feat: checkout page

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,10 +1,46 @@
-import { Navigation } from "../../components/nav/Navigation";
+import { useContext, useState } from "react";
+import { Addresses } from "../../components/Addresses/Addresses";
+import { PriceDetailCard } from "../../components/cards/PriceDetailCard/PriceDetailCard";
+import { DataContext } from "../../contexts/DataContext";
+
+import "./Checkout.css";
+import { OrderSummary } from "../Pages";
 
 export function Checkout() {
+  const [showOrderSummary, setShowOrderSummary] = useState(false);
+  const { dataState } = useContext(DataContext);
+  const { cart, addresses } = dataState;
+  const [selectedAddress, setSelectedAddress] = useState(addresses[0]);
+
+  if (cart.length === 0) {
+    return (
+      <>
+        <h1>Cart is empty.</h1>
+      </>
+    );
+  }
   return (
-    <>
-      <Navigation />
+    <div>
       Checkout
-    </>
+      {showOrderSummary && <OrderSummary selectedAddress={selectedAddress} />}
+      {!showOrderSummary && (
+        <div className="checkout-body">
+          <div>
+            <Addresses
+              checkout
+              selectedAddress={selectedAddress}
+              setSelectedAddress={setSelectedAddress}
+            />
+          </div>
+          <div>
+            <PriceDetailCard
+              cart={cart}
+              setShowOrderSummary={setShowOrderSummary}
+              checkout
+            />
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function Error() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => navigate("/"), 500);
+  });
   return (
     <>
       <h1>Error! Please input correct URL.</h1>
