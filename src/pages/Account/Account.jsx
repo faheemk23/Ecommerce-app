5dbb382184e8211768b2d7a4a5130824f5ff2b5c
faheemk23 <faheemwork23@gmail.com>
2023-05-26 22:08:32 +0530

    feat: style and change account page

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,5 +1,4 @@
 import { useContext, useState } from "react";
-import { Navigation } from "../../components/nav/Navigation";
 import { AuthContext } from "../../contexts/AuthContext";
 import { DataContext } from "../../contexts/DataContext";
 import { ProductsListingContext } from "../../contexts/ProductsListingContext";
@@ -7,15 +6,19 @@ import { useNavigate } from "react-router-dom";
 
 import "./Account.css";
 import { ProfileDetailCard } from "../../components/cards/ProfileDetailCard/ProfileDetailCard";
-import { Addresses } from "../../components/Addresses/Addresses";
+import { AddressCard } from "../../components/cards/AddressCard/AddressCard";
+import { AddressInput } from "../../components/AddressesInput/AddressInput";
 
 export function Account() {
+  const [showAddressInput, setShowAddressInput] = useState(false);
   const [currentCard, setCurrentCard] = useState("profile");
   const { setLoggedIn } = useContext(AuthContext);
   const { dataDispatch, dataState } = useContext(DataContext);
   const { filtersDispatch } = useContext(ProductsListingContext);
 
   const navigate = useNavigate();
+
+  const { addresses } = dataState;
 
   const handleBtnLogout = () => {
     localStorage.clear();
@@ -25,8 +28,8 @@ export function Account() {
     navigate("/");
   };
   return (
-    <>
-      <h2 className="account-heading">Account</h2>
+    <div className="account-outer-container">
+      <h2 className="account-heading">Your Account</h2>
       <div className="account-container">
         <div className="account-headers">
           <button
@@ -39,17 +42,36 @@ export function Account() {
             className="account-header-address"
             onClick={() => setCurrentCard("addresses")}
           >
-            Your Address
+            Your Addresses
           </button>
         </div>
         <div className="account-info">
           {currentCard === "profile" && <ProfileDetailCard />}
-          {currentCard === "addresses" && <Addresses />}
+          {currentCard === "addresses" && (
+            <>
+              {showAddressInput ? (
+                <AddressInput setShowAddressInput={setShowAddressInput} />
+              ) : (
+                <button
+                  className="btn btn-primary"
+                  onClick={() => setShowAddressInput(true)}
+                >
+                  Add new address
+                </button>
+              )}
+              {addresses.map((address) => (
+                <AddressCard address={address} />
+              ))}
+            </>
+          )}
         </div>
       </div>
-      <button className="btn btn-primary" onClick={handleBtnLogout}>
+      <button
+        className="btn btn-secondary btn-logout"
+        onClick={handleBtnLogout}
+      >
         Logout
       </button>
-    </>
+    </div>
   );
 }
