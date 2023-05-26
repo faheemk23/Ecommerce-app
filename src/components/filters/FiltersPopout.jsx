f9086f465f96d544ec6609de10228ec544e46ecb
faheemk23 <faheemwork23@gmail.com>
2023-05-26 22:05:50 +0530

    feat: added responsive filter pop

---
+++ src/components/filters/FiltersPopout.jsx
@@ -0,0 +1,29 @@
+import { Price } from "./Price";
+import { Categories } from "./Categories";
+import { Rating } from "./Rating";
+import { Sort } from "./Sort";
+import { useContext } from "react";
+import { ProductsListingContext } from "../../contexts/ProductsListingContext";
+
+export function FiltersPopout() {
+  const { filtersDispatch } = useContext(ProductsListingContext);
+  return (
+    <div className="filters-popout">
+      <div className="filter-column">
+        <div>
+          <strong>Filters</strong>{" "}
+          <button
+            className="btn btn-primary bg-orange"
+            onClick={() => filtersDispatch({ type: "clear-all" })}
+          >
+            Clear
+          </button>
+        </div>
+        <Price />
+        <Categories />
+        <Rating />
+        <Sort />
+      </div>
+    </div>
+  );
+}
