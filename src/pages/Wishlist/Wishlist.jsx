16f4b7e1ba4c8fc68ab618397032adbeb45b4755
faheemk23 <faheemwork23@gmail.com>
2023-05-26 22:11:57 +0530

    feat: style and change wishlist page

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,17 +1,24 @@
 import { useContext } from "react";
-import { WishlistItemCard } from "../../components/cards/WishlistItemCard/WishlistItemCard";
-import { DataContext } from "../../contexts/DataContext";
 
+import { DataContext } from "../../contexts/DataContext";
 import "./Wishlist.css";
+import { ProductCard } from "../../components/cards/ProductCard/ProductCard";
+import emptyResult from "../../assets/empty-result.jpg";
 
 export function Wishlist() {
   const { dataState } = useContext(DataContext);
   const { wishlist } = dataState;
   if (wishlist.length === 0) {
     return (
-      <>
+      <div className="empty-result-container">
+        <img
+          className="image-center"
+          src={emptyResult}
+          height="292px"
+          width="292px"
+        />
         <h1>wishlist is empty. Please add some items.</h1>
-      </>
+      </div>
     );
   }
 
@@ -19,7 +26,7 @@ export function Wishlist() {
     <div className="wishlist-page">
       <div className="wishlist-items">
         {wishlist.map((product) => (
-          <WishlistItemCard key={product._id} product={product} inCart />
+          <ProductCard product={product} inWishlist />
         ))}
       </div>
     </div>
