f2834b6fb66e81d91af0153116ba078b789c8555
faheemk23 <faheemwork23@gmail.com>
2023-05-26 22:10:48 +0530

    feat : product detail page

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,7 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { ProductCard } from "../../components/cards/ProductCard/ProductCard";
+import { ColorRing } from "react-loader-spinner";
+import { ProductDetailCard } from "../../components/cards/ProductDetailCard/ProductDetailCard";
 
 export function ProductDetail() {
   const [product, setProduct] = useState({});
@@ -9,6 +10,7 @@ export function ProductDetail() {
   const { productId } = useParams();
   const fetchProductDetail = async (productId, setProduct) => {
     try {
+      setLoading(true);
       const res = await axios.get(`/api/products/${productId}`);
       if (res.status === 200) {
         setProduct(res.data.product);
@@ -26,12 +28,24 @@ export function ProductDetail() {
 
   useEffect(() => {
     fetchProductDetail(productId, setProduct);
-  }, []);
+  }, [productId]);
 
   return (
     <>
-      {loading && <p>Loading...</p>}
-      {!loading && <ProductCard product={product} />}
+      {loading && (
+        <div className="center-of-page">
+          <ColorRing
+            visible={true}
+            height="100"
+            width="100"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+          />
+        </div>
+      )}
+      {!loading && <ProductDetailCard product={product} />}
     </>
   );
 }
